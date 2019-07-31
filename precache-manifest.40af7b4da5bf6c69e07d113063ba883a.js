939e3977728841b1b771a4a212737a0d6bc9675b
Alexandra Baer <alexbaer@Alexandras-MacBook-Pro.local>
2019-07-31 09:37:05 -0400

    Updates

---
+++ precache-manifest.40af7b4da5bf6c69e07d113063ba883a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "562db9cc3c17098c0b28874da7ac8447",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "8ed836ec13ed1f6214b5",
+    "url": "/clicky-game/static/css/main.8373fb15.chunk.css"
+  },
+  {
+    "revision": "ed67e21e0fbb680b5f8f",
+    "url": "/clicky-game/static/js/2.abafc83a.chunk.js"
+  },
+  {
+    "revision": "8ed836ec13ed1f6214b5",
+    "url": "/clicky-game/static/js/main.0b9fdf35.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
