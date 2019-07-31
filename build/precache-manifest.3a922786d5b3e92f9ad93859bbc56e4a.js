59476b7c7f27baaee021ad36e6182faf946e72cf
Alexandra Baer <alexbaer@Alexandras-MacBook-Pro.local>
2019-07-31 09:27:11 -0400

    gh pages deploy

---
+++ build/precache-manifest.3a922786d5b3e92f9ad93859bbc56e4a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c24642337837e6b9e5f51770d6c672ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46b56725980751b68244",
+    "url": "/static/css/main.8373fb15.chunk.css"
+  },
+  {
+    "revision": "ed67e21e0fbb680b5f8f",
+    "url": "/static/js/2.abafc83a.chunk.js"
+  },
+  {
+    "revision": "46b56725980751b68244",
+    "url": "/static/js/main.af2598b7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
